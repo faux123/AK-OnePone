7681d2710108a7d934c4341ac22c181d7e385584
anarkia1976 <stefano.villa1976@gmail.com>
2014-09-01 15:56:01 +0200

    ak-bacon-build: switch toolchain 4.8.4-2014.08.20140901.CR83 - A15

--- ak-bacon-build.sh
+++ ak-bacon-build.sh
@@ -22,7 +22,7 @@ AK_VER="$BASE_AK_VER$VER"
 
 # Vars
 export LOCALVERSION=~`echo $AK_VER`
-export CROSS_COMPILE=${HOME}/android/AK-linaro/4.7.4-2014.04.20140418.CR83/bin/arm-cortex_a15-linux-gnueabihf-
+export CROSS_COMPILE=${HOME}/android/AK-linaro/4.8.4-2014.08.20140901.CR83/bin/arm-cortex_a15-linux-gnueabihf-
 export ARCH=arm
 export SUBARCH=arm
 export KBUILD_BUILD_USER=ak
